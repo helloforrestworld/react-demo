f6d2e0c9273053b2e9a86dbc3707d46514399dae
helloforrestworld@163.com <helloforrestworld@163.com>
2019-12-13 21:05:34 +0800

    :hammer: refactor: (Header) replace component state with redux state

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,20 @@
-const defaultState = {}
+const defaultState = {
+  focused: false
+}
 
 export default ( state = defaultState, action ) => {
+  if (action.type === 'search_focus') {
+    return {
+      ...state,
+      focused: true
+    }
+  }
+
+  if (action.type === 'search_blur') {
+    return {
+      ...state,
+      focused: false
+    }
+  }
   return state
 }
