4df4dd7c07230e6b67b849bde0406aa87d374198
helloforrestworld@163.com <helloforrestworld@163.com>
2019-12-19 01:22:09 +0800

    :tada: init a new react demo

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,18 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import App from './App'
+
+const element = (
+  <div>
+    <h1>Hello!</h1>
+    <h2>Good to see you here.</h2>
+  </div>
+);
+
+function Hello () {
+  return element
+}
 
 ReactDOM.render(
-  <App />,
+  <Hello />,
   document.getElementById('root')
 );
--- src/pages/login/store/constants.js
+++
@@ -1,1 +0,0 @@
-export const CHANGE_LOGIN = 'login/change_login'
