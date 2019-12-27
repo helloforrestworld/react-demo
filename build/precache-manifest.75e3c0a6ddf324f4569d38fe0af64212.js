a6151792fd10e10d657f1fc6680ba61b3f5290af
helloforrestworld@163.com <helloforrestworld@163.com>
2019-12-27 15:44:42 +0800

    多页面打包webpack配置

---
+++ build/precache-manifest.75e3c0a6ddf324f4569d38fe0af64212.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d577cdc87345c0cd47c207e1db9c6471",
+    "url": "https://www.cdn.com/index.html"
+  },
+  {
+    "revision": "9aac22aa6acef99b6ea478f53232bcd2",
+    "url": "https://www.cdn.com/order.html"
+  },
+  {
+    "revision": "b73ead9e0323c03a251f561500cece7b",
+    "url": "https://www.cdn.com/query.html"
+  },
+  {
+    "revision": "eb082e4fdbbc6d481a8a",
+    "url": "https://www.cdn.com/static/css/0.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5daa5214f483a74f5f22",
+    "url": "https://www.cdn.com/static/css/index.689279f0.chunk.css"
+  },
+  {
+    "revision": "c2f13d54556ca6dd2a94",
+    "url": "https://www.cdn.com/static/css/order.689279f0.chunk.css"
+  },
+  {
+    "revision": "54bcfadc7c43f099d971",
+    "url": "https://www.cdn.com/static/css/query.689279f0.chunk.css"
+  },
+  {
+    "revision": "87779611478d68c58470",
+    "url": "https://www.cdn.com/static/css/ticket.689279f0.chunk.css"
+  },
+  {
+    "revision": "eb082e4fdbbc6d481a8a",
+    "url": "https://www.cdn.com/static/js/0.eb082e4f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "https://www.cdn.com/static/js/0.eb082e4f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5daa5214f483a74f5f22",
+    "url": "https://www.cdn.com/static/js/index.5daa5214.chunk.js"
+  },
+  {
+    "revision": "c2f13d54556ca6dd2a94",
+    "url": "https://www.cdn.com/static/js/order.c2f13d54.chunk.js"
+  },
+  {
+    "revision": "54bcfadc7c43f099d971",
+    "url": "https://www.cdn.com/static/js/query.54bcfadc.chunk.js"
+  },
+  {
+    "revision": "52e71d3da0379d47bc78",
+    "url": "https://www.cdn.com/static/js/runtime~index.52e71d3d.js"
+  },
+  {
+    "revision": "154ab21d9122c4d556ec",
+    "url": "https://www.cdn.com/static/js/runtime~order.154ab21d.js"
+  },
+  {
+    "revision": "e514fdbadf1ca3111494",
+    "url": "https://www.cdn.com/static/js/runtime~query.e514fdba.js"
+  },
+  {
+    "revision": "0f88acfe4b0f8b7d9944",
+    "url": "https://www.cdn.com/static/js/runtime~ticket.0f88acfe.js"
+  },
+  {
+    "revision": "87779611478d68c58470",
+    "url": "https://www.cdn.com/static/js/ticket.87779611.chunk.js"
+  },
+  {
+    "revision": "dcbda853f8d33f6ac97d8e5809f1ee41",
+    "url": "https://www.cdn.com/ticket.html"
+  }
+]);
